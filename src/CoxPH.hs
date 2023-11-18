02aaf0e1601b7276be958984b520d1867f86a31f
David Pritchard <david.al.pritchard@gmail.com>
2023-11-18 18:04:58 -0500

    add stub 'coxph' function

--- src/CoxPH.hs
+++ src/CoxPH.hs
@@ -1,5 +1,15 @@
 module CoxPH (
-  Delta(..)
+  Delta(..),
+  coxph
   ) where
 
+import qualified Data.Vector.Unboxed as V
+
 data Delta = ObservedEvent | Censored
+
+-- type Vec2DimDouble = V.Vector (V.Vector Double)
+
+newtype CoxPHResult = CoxPHResult (V.Vector Double)
+
+coxph :: V.Vector Double -> V.Vector Delta -> V.Vector (V.Vector Double) -> CoxPHResult
+coxph _ _ _ = CoxPHResult (V.fromList [])
