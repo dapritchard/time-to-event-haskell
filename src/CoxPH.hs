b01c3f072a3b0f29357acb720bc9a3b8d2cea9c9
David Pritchard <david.al.pritchard@gmail.com>
2023-11-26 12:44:09 -0500

    change qualified alias to 'VU'

--- src/CoxPH.hs
+++ src/CoxPH.hs
@@ -7,11 +7,11 @@ module CoxPH (
   coxph
   ) where
 
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as VU
 
 data Delta = ObservedEvent | Censored
 
--- type Vec2DimDouble = V.Vector (V.Vector Double)
+-- type Vec2DimDouble = VU.Vector (VU.Vector Double)
 
 data CoxPHConvergenceFailure = CoxPHConvergenceFailure
 
@@ -21,16 +21,17 @@ data LastInStrataIndicator = LastInStrataNo | LastInStrataYes
 
 data ScaleCovariateIndicator = ScaleCovariateNo | ScaleCovariateYes
 
-type CoxPHResult = Either CoxPHConvergenceFailure (V.Vector Double)
+-- type CoxPHResult = Either CoxPHConvergenceFailure (VU.Vector Double)
+type CoxPHResult = Either Text (VU.Vector Double)
 
-coxph :: V.Vector Double                   -- length n, the per-subject minumum times of the event or censoring times
-      -> V.Vector Delta                    -- length n, indicators for whether patient observered an event or censoring
-      -> V.Vector (V.Vector Double)        -- dimension n by p, X design matrix
-      -> V.Vector Double                   -- length n, X offset vector
-      -> V.Vector Double                   -- length n, subject weights
-      -> V.Vector LastInStrataIndicator    -- length n, indicators for whether a patient had the latest minumum event of censoring time within a strata
-      -> V.Vector Double                   -- length p, starting values for the beta coefficients
-      -> V.Vector ScaleCovariateIndicator  -- length p, indicators for whether a given covariate should be centered and scaled
+coxph :: VU.Vector Double                   -- length n, the per-subject minumum times of the event or censoring times
+      -> VU.Vector Delta                    -- length n, indicators for whether patient observered an event or censoring
+      -> VU.Vector (VU.Vector Double)        -- dimension n by p, X design matrix
+      -> VU.Vector Double                   -- length n, X offset vector
+      -> VU.Vector Double                   -- length n, subject weights
+      -> VU.Vector LastInStrataIndicator    -- length n, indicators for whether a patient had the latest minumum event of censoring time within a strata
+      -> VU.Vector Double                   -- length p, starting values for the beta coefficients
+      -> VU.Vector ScaleCovariateIndicator  -- length p, indicators for whether a given covariate should be centered and scaled
       -> CoxPHMethod                       -- which of the Breslow or Effron methods to use in the event of tied event times
       -> Integer                           -- the maximumn number of Newton-Rhapson iterations to perform
       -> Double                            -- the value for which the absolute value of 1 minus ratio of the likelihood for two consecutive iterations must be below for convergence to be achieved
@@ -48,7 +49,7 @@ coxph _ -- time
       _ -- maxIterations
       _ -- epsilon
       _ = do -- tolerance
-  Right (V.fromList [])
+  Right (VU.fromList [])
 
 calcVecMean :: V.Vector Double -> Either CoxPHConvergenceFailure Double
 calcVecMean x =
