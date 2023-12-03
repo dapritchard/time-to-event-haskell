e36158fb3c64145f985589948fa944a707bd830a
David Pritchard <david.al.pritchard@gmail.com>
2023-12-03 11:43:00 -0500

    improve error handling

--- src/CoxPH.hs
+++ src/CoxPH.hs
@@ -14,7 +14,7 @@ module CoxPH (
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector as V
 import Data.Either (fromRight, isLeft)
-import Data.Text (Text)
+import qualified Data.Text as T
 
 data Delta = ObservedEvent | Censored
 
@@ -29,7 +29,7 @@ data LastInStrataIndicator = LastInStrataNo | LastInStrataYes
 data ScaleCovariateIndicator = ScaleCovariateNo | ScaleCovariateYes
 
 -- type CoxPHResult = Either CoxPHConvergenceFailure (VU.Vector Double)
-type CoxPHResult = Either Text (VU.Vector Double)
+type CoxPHResult = Either T.Text (VU.Vector Double)
 
 coxph :: VU.Vector Double                   -- length n, the per-subject minumum times of the event or censoring times
       -> VU.Vector Delta                    -- length n, indicators for whether patient observered an event or censoring
@@ -58,24 +58,25 @@ coxph _ -- time
       _ = do -- tolerance
   Right (VU.fromList [])
 
--- TODO: include checks that the lengths of the various vectors are consistent?
+-- The error handling could be improved to (i) list all of the errors and (ii)
+-- to list the indices of the failing vectors
 centerAndScaleCovs :: V.Vector (VU.Vector Double)
                    -> VU.Vector Double
                    -> V.Vector ScaleCovariateIndicator
-                   -> Either Text (V.Vector (VU.Vector Double, Double))
+                   -> Either T.Text (V.Vector (VU.Vector Double, Double))
 centerAndScaleCovs xDesignMatrix weights scaleIndicators =
-  let nCovs = length xDesignMatrix
-      nScaleIndicators = length scaleIndicators
+  let nCovs = V.length xDesignMatrix
+      nScaleIndicators = V.length scaleIndicators
       sumWeights = VU.sum weights
   in if nCovs /= nScaleIndicators
      then Left "The length of first input is not the same as the length of the third input"
      else do
        weightedMeans <- calcWeightedMeans sumWeights xDesignMatrix weights scaleIndicators
        let covTuples = V.zip5 xDesignMatrix
-                             (V.replicate nCovs weights)
-                             (V.convert weightedMeans)
-                             (V.replicate nCovs sumWeights)
-                             scaleIndicators
+                              (V.replicate nCovs weights)
+                              (V.convert weightedMeans)
+                              (V.replicate nCovs sumWeights)
+                              scaleIndicators
            eitherResults = V.map conditionallyCenterAndScaleCov covTuples
        if V.any isLeft eitherResults
        then Left "Not all covariates where able to be centered and scaled"
@@ -90,35 +91,48 @@ centerAndScaleCov :: VU.Vector Double
                   -> VU.Vector Double
                   -> Double
                   -> Double
-                  -> Either Text (VU.Vector Double, Double)
-centerAndScaleCov x weights mean sumWeights =
-  let xCentered = VU.map (subtract mean) x
-      covariatePairs = VU.zip xCentered weights
-      weightedAbsCovSum = VU.foldl calcAbsProd 0 covariatePairs
-  in  if weightedAbsCovSum == 0
-      then Left "Constant column"
-      else let scaleVal = sumWeights / weightedAbsCovSum
-           in  Right (VU.map (* scaleVal) xCentered, scaleVal)
+                  -> Either T.Text (VU.Vector Double, Double)
+centerAndScaleCov x weights mean sumWeights
+  | VU.length x /= VU.length weights =
+      Left $ T.concat [ "The length of the first input (the covariate values) "
+                      , "is not the same as the length of the second input "
+                      , "(the weights)"
+                      ]
+  | otherwise =
+      let xCentered = VU.map (subtract mean) x
+          covariatePairs = VU.zip xCentered weights
+          weightedAbsCovSum = VU.foldl calcAbsProd 0 covariatePairs
+      in  if weightedAbsCovSum == 0
+          then Left "Constant column"
+          else let scaleVal = sumWeights / weightedAbsCovSum
+               in  Right (VU.map (* scaleVal) xCentered, scaleVal)
   where
     calcAbsProd :: Double -> (Double, Double) -> Double
     calcAbsProd sumVal (xVal, weightVal) = sumVal + abs (xVal * weightVal)
 
+-- The error handling could be improved to (i) list all of the errors and (ii)
+-- to list the indices of the failing vectors
 calcWeightedMeans :: Double
                   -> V.Vector (VU.Vector Double)
                   -> VU.Vector Double
                   -> V.Vector ScaleCovariateIndicator
-                  -> Either Text (VU.Vector Double)
+                  -> Either T.Text (VU.Vector Double)
 calcWeightedMeans sumWeights xDesignMatrix weights scaleIndicators = do
   let covariatePairs = V.zip xDesignMatrix scaleIndicators
-  let vecEitherMeans = V.map (conditionallyCalcWeightedMean weights)
+      eitherMeans = V.map (conditionallyCalcWeightedMean weights)
                              covariatePairs
-  if V.any isLeft vecEitherMeans
-  then Left "There was an error"  -- TODO: need to make this better
-  else Right (VU.convert (V.map (fromRight 0) vecEitherMeans))
+      errorCovs = V.filter isLeft eitherMeans
+  if V.length errorCovs >= 1
+  then
+    -- The conditional statement makes the `Right` case unreachable
+    case V.head errorCovs of
+      Left x -> Left x
+      Right x -> Right (VU.singleton x)
+  else Right (VU.convert (V.map (fromRight 0) eitherMeans))
   where
     conditionallyCalcWeightedMean :: VU.Vector Double
                                   -> (VU.Vector Double, ScaleCovariateIndicator)
-                                  -> Either Text Double
+                                  -> Either T.Text Double
     conditionallyCalcWeightedMean _ (_, ScaleCovariateNo) = Right 0
     conditionallyCalcWeightedMean weights (x, ScaleCovariateYes) =
       calcWeightedMean x weights sumWeights
@@ -126,7 +140,7 @@ calcWeightedMeans sumWeights xDesignMatrix weights scaleIndicators = do
 calcWeightedMean :: VU.Vector Double
                  -> VU.Vector Double
                  -> Double
-                 -> Either Text Double
+                 -> Either T.Text Double
 calcWeightedMean x weights sumWeights
   | VU.length x == 0 = Left "Can't take the mean of a length-0 vector"
   | sumWeights == 0 = Left "Can't take the mean when the weights sum to 0"
@@ -143,7 +157,7 @@ calcWeightedMean x weights sumWeights
 -- calcWeightedMeans2 :: V.Vector (VU.Vector Double)
 --                   -> VU.Vector Double
 --                   -> V.Vector ScaleCovariateIndicator
---                   -> Either Text (VU.Vector Double)
+--                   -> Either T.Text (VU.Vector Double)
 -- calcWeightedMeans2 xDesignMatrix weights scaleIndicators = do
 --   let covariatePairs = V.zip xDesignMatrix scaleIndicators
 --   let vecEitherMeans = V.map (conditionallyCalcMean weights) covariatePairs
@@ -153,13 +167,13 @@ calcWeightedMean x weights sumWeights
 --   where
 --     conditionallyCalcMean :: VU.Vector Double
 --                           -> (VU.Vector Double, ScaleCovariateIndicator)
---                           -> Either Text Double
+--                           -> Either T.Text Double
 --     conditionallyCalcMean _ (_, ScaleCovariateNo) = Right 0
 --     conditionallyCalcMean weights (x, ScaleCovariateYes) = calcWeightedMean2 x weights
 
 -- calcWeightedMean2 :: VU.Vector Double
 --                  -> VU.Vector Double
---                  -> Either Text Double
+--                  -> Either T.Text Double
 -- calcWeightedMean2 x weights =
 --   if VU.length x == 0
 --   then Left "Can't take the mean of a length-0 vector"
