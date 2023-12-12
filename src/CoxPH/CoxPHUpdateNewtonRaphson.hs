982149ab7af8b9c618f8172a145bc98f866f0cbe
David Pritchard <david.al.pritchard@gmail.com>
2023-12-12 00:40:33 -0500

    start writing 'calcTimeBlock'

--- src/CoxPH/CoxPHUpdateNewtonRaphson.hs
+++ src/CoxPH/CoxPHUpdateNewtonRaphson.hs
@@ -6,18 +6,10 @@ module CoxPH.CoxPHUpdateNewtonRaphson where
 import CoxPH.Data
 -- import Numeric.LinearAlgebra.Data -- (Matrix, Vector, fromColumns)
 import Numeric.LinearAlgebra qualified as L -- (Matrix, Vector, fromColumns)
-import Numeric.LinearAlgebra ( (!), (#>), Matrix, Vector, add, outer, scale, fromColumns )
+import Numeric.LinearAlgebra ( (!), (#>), Matrix, Vector, add, diag, outer, scale, fromColumns )
 import Data.Vector.Storable qualified as VS
 import Data.Vector.Unboxed qualified as VU
 import Data.Vector qualified as V
-import qualified CoxPH.CoxPHUpdateNewtonRaphson as overallData
-
-data StrataData = StrataData {
-    time :: VS.Vector Double
-  , eventStatus :: V.Vector Delta
-  , xDesignMatrix :: Matrix Double
-  , xOffset :: Vector Double
-  }
 
 updateNewtonRaphson :: [StrataData] -> VS.Vector Double -> Vector Double -> ()
 updateNewtonRaphson strataDatas weights beta =
@@ -42,12 +34,19 @@ data IterationInfo = IterationInfo {
   , time :: Double
   }
 
+data StrataData = StrataData {
+    time :: VS.Vector Double
+  , eventStatus :: V.Vector Delta
+  , xDesignMatrix :: Matrix Double
+  , xOffset :: Vector Double
+  }
+
 data OverallData = OverallData {
     sumWeightedRisk :: Double
   , logLikelihood :: Double
   , score :: Vector Double
   , xBarUnscaled :: Vector Double
-  , informationUnscaled :: Matrix Double
+  , informationTerm1 :: Matrix Double
   }
 
 data TiedData = TiedData {
@@ -56,62 +55,104 @@ data TiedData = TiedData {
   , logLikelihood :: Double
   , score :: Vector Double
   , xBarUnscaled :: Vector Double
-  , informationUnscaled :: Matrix Double
+  , informationTerm1 :: Matrix Double
   }
 
-calcTimeBlock :: Int -> Int -> VS.Vector Double -> ()
-calcTimeBlock nSubjects subjectIndex xProdBeta
-  | subjectIndex >= nSubjects = ()
-  | otherwise =
-    ()
+calcTimeBlock
+  :: StrataData
+  -> IterationInfo
+  -> Vector Double
+  -> OverallData
+  -- -> TiedData
+  -> (IterationInfo, OverallData)
+-- calcTimeBlock strataData iterationInfo weightedRisk overallData tiedData
+calcTimeBlock strataData iterationInfo weightedRisk overallData =
+  let p = VS.length overallData.score
+      tiedData = TiedData {
+        sumWeights = 0
+        , sumWeightedRisk = 0
+        , logLikelihood = 0
+        , score = VS.replicate p 0
+        , xBarUnscaled = VS.replicate p 0
+        , informationTerm1 = createEmptyMatrix p
+        }
+      (newIterationInfo, newOverallData, newTiedData) =
+        calcSubject
+          strataData iterationInfo weightedRisk overallData newTiedData
+      -- newSumWeightedRisk = newOverallData.sumWeightedRisk + newTiedData.sumWeightedRisk
+      -- newLogLikelihood = newOverallData.logLikelihood - (newTiedData.sumWeights * log newTiedData.logLikelihood)
+      newXBarUnscaled = newOverallData.xBarUnscaled + newTiedData.xBarUnscaled
+      newXBar = scale newTiedData.sumWeights newXBarUnscaled
+      -- newScore = add newOverallData.score
+      --                (scale newTiedData.sumWeights newXBar)
+      -- newInformationTerm1 = add newOverallData.informationTerm1
+      --                           newTiedData.informationTerm1
+      updatedOverallData = {
+          sumWeightedRisk = newOverallData.sumWeightedRisk + newTiedData.sumWeightedRisk
+        , logLikelihood = newOverallData.logLikelihood
+                          - (newTiedData.sumWeights
+                             * log newTiedData.logLikelihood)
+        , score = add newOverallData.score
+                      (scale newTiedData.sumWeights newXBar)
+        , xBarUnscaled = newXBarUnscaled
+        , informationTerm1 = add newOverallData.informationTerm1
+                                 newTiedData.informationTerm1
+        }
+  in  (newIterationInfo, updatedOverallData)
 
--- calcSubject :: Int -> Int -> Int -> Double -> Vector Double -> StrataData -> OverallData -> TiedData -> ()
--- calcSubject nSubjects subjectIndex nDead time weightedRisk strataData overallData tiedData
-calcSubject :: IterationInfo -> Vector Double -> StrataData -> OverallData -> TiedData
-               -> (IterationInfo, StrataData, OverallData, TiedData)
-calcSubject iterationInfo weightedRisk strataData overallData tiedData
-  -- case: we've either seen all of the subjects or we've found a subject with a
+calcSubject
+  :: StrataData
+  -> IterationInfo
+  -> Vector Double
+  -> OverallData
+  -> TiedData
+  -> (IterationInfo, OverallData, TiedData)
+calcSubject strataData iterationInfo weightedRisk overallData tiedData
+  -- Case: we've either seen all of the subjects or we've found a subject with a
   -- different censoring or event time. This is the base case
   | (iterationInfo.subjectIndex >= iterationInfo.nSubjects)
-    || ((strataData.time ! iterationInfo.subjectIndex) /= iterationInfo.time) =
-      (iterationInfo, strataData, overallData, tiedData)
-  -- case: TODO
+      || (strataData.time ! iterationInfo.subjectIndex) /= iterationInfo.time =
+      (iterationInfo, overallData, tiedData)
+  -- Case: the current subject is part of the censoring or event tied time block
+  -- (note that it could be the first subject in the block)
   | otherwise =
       let subjectWeightedRisk = weightedRisk ! iterationInfo.subjectIndex
           subjectXRow = strataData.xDesignMatrix ! iterationInfo.subjectIndex
           weightedSubjectXRow = scale subjectWeightedRisk subjectXRow
-          subjectCovariateTerm = scale subjectWeightedRisk
-                                       (outer subjectXRow
-                                              subjectXRow)
+          newInformationTerm1 = scale subjectWeightedRisk
+                                      (outer subjectXRow subjectXRow)
       in  case strataData.eventStatus V.! iterationInfo.subjectIndex of
             Censored ->
-              let newSumWeightedRisk = (overallData.sumWeightedRisk
-                                        + subjectWeightedRisk)
-                  newXBarUnscaled = add overallData.xBarUnscaled
+              let newOverallData = OverallData {
+                    sumWeightedRisk = overallData.sumWeightedRisk
+                                      + subjectWeightedRisk
+                    , logLikelihood = overallData.logLikelihood
+                    , score = overallData.score
+                    , xBarUnscaled = add overallData.xBarUnscaled
                                         weightedSubjectXRow
-                  newOverallData = OverallData {
-                        sumWeightedRisk = newSumWeightedRisk
-                      , logLikelihood = overallData.logLikelihood
-                      , score = overallData.score
-                      , xBarUnscaled = newXBarUnscaled
-                      , informationUnscaled = overallData.informationUnscaled
+                    , informationTerm1 = newInformationTerm1
                     }
-              in  (iterationInfo, strataData, newOverallData, tiedData)
+                  newIterationInfo = iterationInfo {
+                      subjectIndex = iterationInfo.subjectIndex + 1
+                    }
+              in  (newIterationInfo, newOverallData, tiedData) -- FIXME recursive call
             ObservedEvent ->
-              let newSumWeights = tiedData.sumWeights + subjectWeightedRisk
-                  newSumWeightedRisk = tiedData.sumWeights + subjectWeightedRisk
-                  newLogLikelihood = tiedData.logLikelihood + subjectWeightedRisk
-                  newScore = add tiedData.score subjectXRow
-                  newXBarUnscaled = add tiedData.xBarUnscaled weightedSubjectXRow
-                  newTiedData = tiedData {
-                          sumWeights = newSumWeights
-                        , sumWeightedRisk = newSumWeightedRisk
-                        , logLikelihood = newLogLikelihood
-                        , score = newScore
-                        , xBarUnscaled = newXBarUnscaled
-                        , informationUnscaled = tiedData.informationUnscaled
-                        }
-              in  (iterationInfo, strataData, overallData, newTiedData)
+              let newTiedData = TiedData {
+                      sumWeights = tiedData.sumWeights + subjectWeightedRisk
+                    , sumWeightedRisk = tiedData.sumWeights
+                                        + subjectWeightedRisk
+                    , logLikelihood = tiedData.logLikelihood
+                                      + subjectWeightedRisk
+                    , score = add tiedData.score subjectXRow
+                    , xBarUnscaled = add tiedData.xBarUnscaled
+                                         weightedSubjectXRow
+                    , informationTerm1 = newInformationTerm1
+                    }
+                  newIterationInfo = iterationInfo {
+                      nDead = iterationInfo.nDead + 1
+                    , subjectIndex = iterationInfo.subjectIndex + 1
+                    }
+              in  (newIterationInfo, overallData, newTiedData) -- FIXME recursive call
 
 -- addSubject :: VS.Vector Double -> Int -> ()
 
@@ -120,7 +161,7 @@ calcSubject iterationInfo weightedRisk strataData overallData tiedData
 --   weightedRisk
 --   logLikelihood
 --   score
---   informationUnscaled
+--   informationTerm1
 
 -- testMatrix :: Matrix Double
 -- testMatrix = matrix 2 [1..4]
@@ -136,3 +177,6 @@ m = fromColumns [v1, v2]
 
 -- testResult :: Vector Double
 -- testResult =  testMatrix #> testVector
+
+createEmptyMatrix :: Int -> Matrix Double
+createEmptyMatrix p = diag (VS.replicate p 0)
