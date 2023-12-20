19abcb699abfe72d695152614e283561c93381ae
David Pritchard <david.al.pritchard@gmail.com>
2023-12-20 11:45:06 -0500

    update types in the tests

--- test/TestCoxPH.hs
+++ test/TestCoxPH.hs
@@ -4,9 +4,10 @@ module Main (main) where
 
 import Control.Monad ( unless )
 import CoxPH.CenterAndScale
+import TestCoxPH.TestCoxPHUpdateNewtonRaphson
 import CoxPH.Data
-import Data.Vector.Unboxed qualified as VU
 import Data.Vector qualified as V
+import Data.Vector.Storable qualified as VS
 import Data.Text (Text)
 import Test.Tasty ( TestTree, defaultMain, testGroup )
 import Test.Tasty.HUnit ( Assertion, testCase, (@?=), assertFailure )
@@ -30,8 +31,8 @@ eps :: Double
 eps = 0.0000000001
 
 -- Compare two objects of the same shape as created by `centerAndScaleCovs`
-assertEqualCovs :: Either Text (V.Vector (VU.Vector Double, Double))
-                -> Either Text (V.Vector (VU.Vector Double, Double))
+assertEqualCovs :: Either Text (V.Vector (VS.Vector Double, Double))
+                -> Either Text (V.Vector (VS.Vector Double, Double))
                 -> Assertion
 assertEqualCovs (Left _) (Right _) = assertFailure "Should be a `Right`"
 assertEqualCovs (Right _) (Left _) = assertFailure "Should be a `Left`"
@@ -46,10 +47,11 @@ assertEqualCovs (Right actual) (Right expected)
   where
     checkEqualScales :: (a1, Double) -> (a2, Double) -> Bool
     checkEqualScales (_, v1) (_, v2) = approxEqual eps v1 v2
-    checkEqualCovs :: (VU.Vector Double, a1) -> (VU.Vector Double, a2) -> Bool
+    checkEqualCovs :: (VS.Vector Double, a1) -> (VS.Vector Double, a2) -> Bool
     checkEqualCovs (x1, _) (x2, _)
-      | VU.length x1 /= VU.length x2 = False
-      | otherwise = VU.all (uncurry (approxEqual eps)) (VU.zip x1 x2)
+      | VS.length x1 /= VS.length x2 = False
+      | otherwise = V.all (uncurry (approxEqual eps))
+                          (V.zip (V.convert x1) (V.convert x2))
 
 -- Compare two numbers for equality up to the specified level of precision
 approxEqual :: Double -> Double -> Double -> Bool
@@ -58,20 +60,20 @@ approxEqual eps x y = abs (x - y) <= eps
 
 -- Create test data ------------------------------------------------------------
 
-x1 :: VU.Vector Double
-x1 = VU.fromList [1, 2, 3, 4, 5, 6]
+x1 :: VS.Vector Double
+x1 = VS.fromList [1, 2, 3, 4, 5, 6]
 
-x2 :: VU.Vector Double
-x2 = VU.fromList [0, 1, 1, 0, 0, 2]
+x2 :: VS.Vector Double
+x2 = VS.fromList [0, 1, 1, 0, 0, 2]
 
-covs :: V.Vector (VU.Vector Double)
+covs :: V.Vector (VS.Vector Double)
 covs = V.fromList [x1, x2]
 
-constWeights :: VU.Vector Double
-constWeights = VU.fromList [1, 1, 1, 1, 1, 1]
+constWeights :: VS.Vector Double
+constWeights = VS.fromList [1, 1, 1, 1, 1, 1]
 
-nonConstWeights :: VU.Vector Double
-nonConstWeights = VU.fromList [1, 2, 3, 4, 5, 6]
+nonConstWeights :: VS.Vector Double
+nonConstWeights = VS.fromList [1, 2, 3, 4, 5, 6]
 
 scaleIndicators :: V.Vector ScaleCovariateIndicator
 scaleIndicators = V.fromList [ScaleCovariateYes, ScaleCovariateNo]
@@ -79,12 +81,12 @@ scaleIndicators = V.fromList [ScaleCovariateYes, ScaleCovariateNo]
 
 -- Create tests ----------------------------------------------------------------
 
-actualConstWeights :: Either Text (V.Vector (VU.Vector Double, Double))
+actualConstWeights :: Either Text (V.Vector (VS.Vector Double, Double))
 actualConstWeights = centerAndScaleCovs covs constWeights scaleIndicators
 
-expectedConstWeights :: Either Text (V.Vector (VU.Vector Double, Double))
+expectedConstWeights :: Either Text (V.Vector (VS.Vector Double, Double))
 expectedConstWeights =
-  let newX1 = VU.fromList
+  let newX1 = VS.fromList
         [ -1.6666666666666665
         , -1
         , -0.3333333333333333
@@ -98,12 +100,12 @@ expectedConstWeights =
         ]
   in  Right expected
 
-actualNonConstWeights :: Either Text (V.Vector (VU.Vector Double, Double))
+actualNonConstWeights :: Either Text (V.Vector (VS.Vector Double, Double))
 actualNonConstWeights = centerAndScaleCovs covs nonConstWeights scaleIndicators
 
-expectedNonConstWeights :: Either Text (V.Vector (VU.Vector Double, Double))
+expectedNonConstWeights :: Either Text (V.Vector (VS.Vector Double, Double))
 expectedNonConstWeights =
-  let newX1 = VU.fromList
+  let newX1 = VS.fromList
         [ -2.6250000000000000
         , -1.8374999999999999
         , -1.0499999999999998
